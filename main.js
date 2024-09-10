fc979494672bcecc1bc3fd9e2d1a71a59d2638bc
paolok1 <gpaolok1@gmail.com>
2024-09-10 14:21:44 +0200

    selfwork4_terminato

---
+++ main.js
@@ -0,0 +1,13 @@
+let cats = 44 ;
+let fila = 6 ;
+let divis = cats / fila;
+let result_divis = cats % fila;
+console.log(divis);
+console.log(result_divis);
+console.log("Ci sono 7 file di gatti e ne mancano 5 per una nuova fila, con un avanzo di 2");
+
+
+
+
+
+ 
