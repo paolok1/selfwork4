1d481b3fd00d31e3e215b73a95ed59254fa0bb0c
paolok1 <gpaolok1@gmail.com>
2024-09-10 15:19:00 +0200

    selfwork_4 aggiornato

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 let cats = 44 ;
 let fila = 6 ;
-let divis = cats / fila;
+let divis = Math.floor(cats / fila) ;
 let result_divis = cats % fila;
+let gattiFuori = divis - result_divis;
 console.log(divis);
 console.log(result_divis);
-console.log("Ci sono 7 file di gatti e ne mancano 5 per una nuova fila, con un avanzo di 2");
+console.log(gattiFuori);
+
+console.log( `Ci sono ${divis} file di gatti e ne mancano ${gattiFuori} per una nuova fila, con un avanzo di ${result_divis}`);
 
 
 
